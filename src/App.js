1ebe5e08dd091a0fa2e6a1dcae72fd06f6257aa9
Quentin Marceron <proquentinmarce@gmail.com>
2023-08-31 16:15:23 +0200

    Add Github Pages + CNAME + Github Workflow

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          La Baraquette de Thau
         </a>
       </header>
     </div>
