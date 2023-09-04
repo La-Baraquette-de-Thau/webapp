f1794aa861f028935357064405730d852fb0c8eb
Quentin Marceron <proquentinmarce@gmail.com>
2023-09-05 00:03:44 +0200

    Add mobile compatibility for the 'Arround' page

--- src/components/Pages/Around/Around.js
+++ src/components/Pages/Around/Around.js
@@ -1,5 +1,5 @@
 
-import {Stack, Tabs, Tab, useMediaQuery} from "@mui/material";
+import {Stack, Tabs, Tab, useMediaQuery, MenuItem, FormControl, InputLabel, Select} from "@mui/material";
 import Map from "../../Map/Map";
 import {useState} from "react";
 import {mapData} from "./mapData";
@@ -10,18 +10,34 @@ export default function Around() {
     const [map, setMap] = useState(mapData[0]);
 
     return (
-        <Stack flexDirection={isDesktop ? 'row' : 'column'} my={'1em'} width={'100%'} sx={{maxWidth: '1024px'}}>
-            <Tabs
-                orientation={isDesktop ? 'vertical' : 'horizontal'}
-                variant="scrollable"
-                value={value}
-                onChange={(_, newValue) => setValue(newValue)}
-                sx={{ borderRight: 1, borderColor: 'divider', width: isDesktop ? '300px' : '100%'}}
-            >
-                {mapData.map((item) =>
-                    <Tab key={item.value} label={item.name} id={item.value} onClick={() => setMap(item)}/>
-                )}
-            </Tabs>
+        <Stack flexDirection={isDesktop ? 'row' : 'column'} alignItems={'center'} my={'1em'} width={'100%'} sx={{maxWidth: '1024px'}}>
+            {isDesktop
+                ?
+                    <Tabs
+                        orientation={'vertical'}
+                        variant="scrollable"
+                        value={value}
+                        onChange={(_, newValue) => setValue(newValue)}
+                        sx={{ borderRight: 1, borderColor: 'divider', width: isDesktop ? '300px' : '100%'}}
+                    >
+                        {mapData.map((item) =>
+                            <Tab key={item.value} label={item.name} id={item.value} onClick={() => setMap(item)}/>
+                        )}
+                    </Tabs>
+                :
+                    <Select
+                        value={map.value}
+                        variant={'standard'}
+                        onChange={(event) => setMap(mapData[event.target.value])}
+                        sx={{width: '85%', mb: 2}}
+                    >
+                        {mapData.map((item) =>
+                            <MenuItem key={item.value} value={item.value}>
+                                {item.name}
+                            </MenuItem>
+                        )}
+                    </Select>
+            }
             <Stack width={'100%'} height={'500px'}>
                 <Map data={map} />
             </Stack>
