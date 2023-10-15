14065568063bf64b0bc2a27a7f68f2295de114dd
Quentin Marceron <proquentinmarce@gmail.com>
2023-10-15 20:57:15 +0200

    fix warning key props for equipment component

--- src/components/Pages/Equipment/Equipment.js
+++ src/components/Pages/Equipment/Equipment.js
@@ -5,19 +5,19 @@ import {equipmentData} from "./equipmentData";
 export default function Equipment() {
 
     return (
-        <Stack width={'100%'} alignItems={'center'} sx={{paddingY: '20px'}}>
+        <Stack width={'100%'} sx={{paddingY: '20px'}}>
             {equipmentData.map(category =>
-                <>
+                <Stack key={category.name} alignItems={'center'}>
                     <Divider flexItem variant={'middle'}><Chip label={category.name} /></Divider>
                     <List sx={{width: '90%'}}>
                         {category.list.map(equipment =>
-                            <ListItem>
+                            <ListItem key={equipment.name}>
                                 <ListItemIcon>{equipment.icon && equipment.icon}</ListItemIcon>
                                 <ListItemText primary={equipment.name}/>
                             </ListItem>
                         )}
                     </List>
-                </>
+                </Stack>
             )}
         </Stack>
     );
